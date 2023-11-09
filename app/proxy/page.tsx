2e016337350523217cc21f8f0744ece0ef79b62f
Michael Ryaboy <sfr.michael@yahoo.com>
2023-11-09 15:19:01 -0800

    adds new urls

--- app/proxy/page.tsx
+++ app/proxy/page.tsx
@@ -34,22 +34,7 @@ type PageData = {
 
 async function getData(url: string) {
   const res = await fetch(
-    `http://localhost:3001/api/getCache?url=${encodeURIComponent(url)}`
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-async function getSummary(url: string) {
-  const res = await fetch(
-    `http://localhost:3001/api/getCache?url=${encodeURIComponent(url)}`
+    `${process.env.NEXT_PUBLIC_API_URL}/getCache?url=${encodeURIComponent(url)}`
   );
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
