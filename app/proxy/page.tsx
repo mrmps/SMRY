bb9afb7cc33fc19d09a2944a0452310164d561c2
Michael Ryaboy <sfr.michael@yahoo.com>
2023-11-14 01:56:11 -0800

    minimum length for summary increased

--- app/proxy/page.tsx
+++ app/proxy/page.tsx
@@ -179,7 +179,7 @@ async function Wrapper({ siteText, url, ip }: { siteText: string; url: string, i
     return cached;
   }
 
-  if (prompt.length < 400) {
+  if (prompt.length < 1000) {
     return "Content too short to summarize."
   }
 
