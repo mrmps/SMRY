2b9efa2de02c973bf19af6ac63b5657875ead14d
Michael Ryaboy <sfr.michael@yahoo.com>
2023-11-15 02:18:19 -0800

    fixes mobile

--- app/proxy/page.tsx
+++ app/proxy/page.tsx
@@ -62,7 +62,6 @@ export default async function Page({
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-
   const headersList = headers();
   const ip = headersList.get("x-real-ip") || "default_ip";
 
@@ -153,7 +152,9 @@ export default async function Page({
                 />
               }
             >
-              <div dangerouslySetInnerHTML={{ __html: content.flattenedHTML }} />
+              <div
+                dangerouslySetInnerHTML={{ __html: content.flattenedHTML }}
+              />
               {/* <div dangerouslySetInnerHTML={{ __html: content.content }} /> */}
               {/* {parse(content.content)} */}
               {/* <div>
@@ -167,9 +168,40 @@ export default async function Page({
   );
 }
 
+const models = [
+  { name: "mistral-7b-instruct", rateLimit: 250 },
+  { name: "openhermes-2-mistral-7b", rateLimit: 250 },
+  { name: "openhermes-2.5-mistral-7b", rateLimit: 250 },
+  { name: "llama-2-13b-chat", rateLimit: 150 },
+  { name: "codellama-34b-instruct", rateLimit: 150 },
+  { name: "llama-2-70b-chat", rateLimit: 60 }
+];
+
+// Calculate total rate limits
+const totalRateLimit = models.reduce((acc, model) => acc + model.rateLimit, 0);
+
+function pickRandomModel() {
+  let randomNum = Math.random() * totalRateLimit;
+  for (let i = 0; i < models.length; i++) {
+    if (randomNum < models[i].rateLimit) {
+      return models[i].name;
+    }
+    randomNum -= models[i].rateLimit;
+  }
+}
+
 // We add a wrapper component to avoid suspending the entire page while the OpenAI request is being made
-async function Wrapper({ siteText, url, ip }: { siteText: string; url: string, ip: string }) {
-  const prompt = "Summarize the following in under 300 words: " + siteText + "Summary:";
+async function Wrapper({
+  siteText,
+  url,
+  ip,
+}: {
+  siteText: string;
+  url: string;
+  ip: string;
+}) {
+  const prompt =
+    "Summarize the following in under 300 words: " + siteText + "Summary:";
 
   // See https://sdk.vercel.ai/docs/concepts/caching
   const cached = (await kv.get(url)) as string | undefined;
@@ -180,7 +212,7 @@ async function Wrapper({ siteText, url, ip }: { siteText: string; url: string, i
   }
 
   if (prompt.length < 2700) {
-    return "Content too short to summarize."
+    return "Content too short to summarize.";
   }
 
   if (
@@ -188,14 +220,13 @@ async function Wrapper({ siteText, url, ip }: { siteText: string; url: string, i
     process.env.KV_REST_API_URL &&
     process.env.KV_REST_API_TOKEN
   ) {
-
     const ratelimit = new Ratelimit({
       redis: kv,
       limiter: Ratelimit.slidingWindow(10, "1 d"),
     });
 
     const { success, limit, reset, remaining } = await ratelimit.limit(
-      `13ft_ratelimit_${ip}`,
+      `13ft_ratelimit_${ip}`
     );
 
     if (!success) {
@@ -227,33 +258,59 @@ async function Wrapper({ siteText, url, ip }: { siteText: string; url: string, i
     //   },
     // });
 
-    const options = {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        Authorization: `Bearer ${process.env.TOGETHER_API_KEY}`
-      },
-      body: JSON.stringify({
-        model: 'togethercomputer/llama-2-7b-chat',
-        prompt: decodedText,
-        max_tokens: 700,
-        temperature: 0.7,
-        top_p: 0.7,
-        top_k: 50,
-        repetition_penalty: 1,
-        stream_tokens: true
-      })
-    };
-    
+    // const options = {
+    //   method: 'POST',
+    //   headers: {
+    //     accept: 'application/json',
+    //     'content-type': 'application/json',
+    //     Authorization: `Bearer ${process.env.TOGETHER_API_KEY}`
+    //   },
+    //   body: JSON.stringify({
+    //     model: 'togethercomputer/llama-2-7b-chat',
+    //     prompt: decodedText,
+    //     max_tokens: 700,
+    //     temperature: 0.7,
+    //     top_p: 0.7,
+    //     top_k: 50,
+    //     repetition_penalty: 1,
+    //     stream_tokens: true
+    //   })
+    // };
+
     // fetch('https://api.together.xyz/inference', options)
     //   .then(response => response.json())
     //   .then(response => console.log(response))
     //   .catch(err => console.error(err));
 
-    const response = await fetch('https://api.together.xyz/inference', options)
+    // const response = await fetch('https://api.together.xyz/inference', options)
 
-        // Convert the response into a friendly text-stream
+    const response = await fetch("https://api.perplexity.ai/chat/completions", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.PERPLEXITY_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: pickRandomModel(),
+        stream: true,
+        max_tokens: 700,
+        frequency_penalty: 1,
+        temperature: 0.0,
+        messages: [
+          {
+            role: "system",
+            content: "Be precise and concise in your responses.",
+          },
+          {
+            role: "user",
+            content: decodedText,
+          },
+        ],
+      }),
+    });
+
+    // Convert the response into a friendly text-stream
     const stream = OpenAIStream(response, {
       async onCompletion(completion) {
         await kv.set(url, completion);
@@ -262,11 +319,11 @@ async function Wrapper({ siteText, url, ip }: { siteText: string; url: string, i
     });
 
     if (!response.ok) {
-      return "Well this sucks. Looks like I ran out of money to pay OpenAI for summaries. Please be patient until a benevolent sponsor gives me either cash or sweet sweet openAI credits. If you would like to be that sponsor, feel free to reach out to vercelsucks@gmail.com (this project is hosted on vercel I just couldn't think of another email.)"
+      return "Well this sucks. Looks like I ran out of money to pay OpenAI for summaries. Please be patient until a benevolent sponsor gives me either cash or sweet sweet openAI credits. If you would like to be that sponsor, feel free to reach out to vercelsucks@gmail.com (this project is hosted on vercel I just couldn't think of another email.)";
     }
 
     return <Tokens stream={stream} />;
   } catch (error) {
-    return `Well this sucks. Looks like an unexpected error occured, so no summary for this site :( No I won't tell you the error, that is private. Really, you insist? Fine. The error is ${error} Happy?`
+    return `Well this sucks. Looks like an unexpected error occured, so no summary for this site :( No I won't tell you the error, that is private. Really, you insist? Fine. The error is ${error} Happy?`;
   }
 }
