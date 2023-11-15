fe3c3fe47e202386c5d1417049866252aa3930b5
Michael Ryaboy <sfr.michael@yahoo.com>
2023-11-14 19:10:02 -0800

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head> */}
       <body
         className={GeistSans.className}
         // style={{background: "#E5EDF0"}}
