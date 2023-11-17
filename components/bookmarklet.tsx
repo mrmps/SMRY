5474c1af85c488cc9acbcf2be00e1df0c05bceb8
Michael Ryaboy <sfr.michael@yahoo.com>
2023-11-16 16:22:19 -0800

    bookmark

--- components/bookmarklet.tsx
+++ components/bookmarklet.tsx
@@ -4,13 +4,13 @@ export const BookmarkletComponent = () => {
         event.dataTransfer.setData('text/plain', "javascript:(function(){window.location.href='https://smry.ai/'+window.location.href;})();");
       };
   return (
-    <section className="hidden sm:block relative z-0 mt-16 bg-gray-100 rounded-xl shadow-sm border-zinc-200 hover:bg-black transition-all duration-500">
+    <section className="hidden sm:block relative z-0 mt-16 bg-white rounded-xl border shadow-md border-zinc-200 hover:bg-black transition-all duration-500">
       <div className="w-full max-w-5xl mx-auto px-4 md:px-6 py-4 rounded-xl">
         <div className="text-center">
           <div
             className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 items-center justify-center text-xl sm:text-2xl md:text-3xl font-semibold text-slate-900 hover:text-white before:absolute before:inset-0 before:rounded-xl hover:before:bg-black before:-z-10 before:transition-colors before:duration-500 group"
           >
-            <span className="relative p-0.5 rounded-full bg-slate-200 group-hover:bg-black transition duration-500 overflow-hidden flex items-center justify-center before:opacity-0 group-hover:before:opacity-100 before:absolute before:w-1/3 before:pb-[100%] before:rounded-full before:bg-[linear-gradient(90deg,_theme(colors.indigo.500/0)_0%,_theme(colors.indigo.500)_35%,_theme(colors.indigo.200)_50%,_theme(colors.indigo.500)_65%,_theme(colors.indigo.500/0)_100%)] before:animate-[spin_3s_linear_infinite]">
+            <span className="relative p-0.5 rounded-full bg-slate-200 group-hover:bg-zinc-700 transition duration-500 overflow-hidden flex items-center justify-center before:opacity-0 group-hover:before:opacity-100 before:absolute before:w-1/3 before:pb-[100%] before:rounded-full before:bg-[linear-gradient(90deg,_theme(colors.indigo.500/0)_0%,_theme(colors.indigo.500)_35%,_theme(colors.indigo.200)_50%,_theme(colors.indigo.500)_65%,_theme(colors.indigo.500/0)_100%)] before:animate-[spin_3s_linear_infinite]">
               <span className="relative whitespace-nowrap ">
                 <span className="block px-6 py-4 rounded-full bg-gradient-to-r from-slate-200 to-slate-100 z-10 group-hover:opacity-0 transition-opacity duration-500 ease-in-out">
                   Quick Summarize
