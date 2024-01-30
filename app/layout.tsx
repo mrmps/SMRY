c33367a586686d26f5397fc15c79efbd465fdb06
Michael Ryaboy <sfr.michael@yahoo.com>
2024-01-30 15:21:54 -0800

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Smry: AI Summarizer and Free Paywall Remover", 
+  title: "Smry | AI Summarizer and Free Paywall Remover", 
   description:
-    "Discover SMRY: an AI tool that not only summarizes long articles for quick comprehension but also skillfully navigates through paywalls, offering rapid access to restricted content.",
+    "SMRY, remove paywalls and summarize articles for free with no login. Supports NYT, Washington Post, and thousands more.",
 };
 
 export default function RootLayout({
