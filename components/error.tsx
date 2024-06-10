2db22b41cb13f982ef235abe5ccad8b1d76075bb
Michael Ryaboy <sfr.michael@yahoo.com>
2024-06-10 11:03:13 -0700

    error boundary

--- components/error.tsx
+++ components/error.tsx
@@ -1,32 +1,35 @@
-import React from 'react'
+import { Component, ErrorInfo, ReactNode } from 'react';
 
-class ErrorBoundary extends React.Component<any, { hasError: boolean }> {
-    constructor(props: any) {
-      super(props)
-   
-      // Define a state variable to track whether is an error or not
-      this.state = { hasError: false }
-    }
-    static getDerivedStateFromError(error: any) {
-      // Update state so the next render will show the fallback UI
-   
-      return { hasError: true }
-    }
-    componentDidCatch(error: any, errorInfo: any) {
-      // You can use your own error logging service here
-      console.log({ error, errorInfo })
-    }
-    render() {
-      // Check if the error is thrown
-      if (this.state.hasError) {
-        // You can render any custom fallback UI
-        return this.props.fallback;
-      }
-   
-      // Return children components in case of no error
-   
-      return this.props.children
-    }
+interface Props {
+  children: ReactNode;
+  fallback: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
   }
-   
-  export default ErrorBoundary
+
+  static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('ErrorBoundary caught an error', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
